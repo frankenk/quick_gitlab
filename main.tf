47490a81b38c2b76e0be758151b3b85d1eb0bbc4
laurym <laurynas.matusevicius@kone.com>
2023-02-10 14:47:55 +0200

    Fixed incorrect naming

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 module "gitlab_instance" {
-  source        = "./modules/instances/"
-  instance_type = "t2.medium"
-  instance_number = 1
-  subnet_id     = var.subnet_id 
-  vpc_id = var.vpc_id
+  source          = "./modules/instances/"
+  instance_type   = "t2.medium"
+  instance_number = 2
+  subnet_id       = var.subnet_id
+  vpc_id          = var.vpc_id
 }
 
 output "public_ip_address" {
-    value = module.gitlab_instance.public_ip_address
+  value = module.gitlab_instance.public_dns_address
 }
 
 output "instance_root_password" {
-    value = module.gitlab_instance.generated_root_password
+  value = module.gitlab_instance.generated_root_password
 }
--- variables.tf
+++ variables.tf
@@ -5,12 +5,12 @@ variable "aws_region" {
 
 variable "subnet_id" {
   description = "Change to your desired subnet - leave empty for default"
-  type = string
-  default = "" #<<<<
+  type        = string
+  default     = "" #<<<<
 }
 
 variable "vpc_id" {
   description = "Change to your desired VPC ID - leave empty for default"
-  type = string
-  default = "" #<<<<
+  type        = string
+  default     = "" #<<<<
 }
